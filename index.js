76d1e883e8035df8166c337d77dcc32d332f1cda
Martin Flischman <26260699+martinflischman@users.noreply.github.com>
2026-05-14 16:54:17 +0200

    refactor: rename openai client to aiClient and remove dangerouslyAllowBrowser

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import OpenAI from "openai";
 import { checkEnvironment } from "./utils.js";
 
-const openai = new OpenAI({
+const aiClient = new OpenAI({
   apiKey: process.env.AI_KEY,
   baseURL: process.env.AI_URL,
-  dangerouslyAllowBrowser: true,
 });
 
 checkEnvironment();
@@ -17,7 +16,7 @@ const userMessage = {
   content: userPrompt,
 };
 
-const response = await openai.chat.completions.create({
+const response = await aiClient.chat.completions.create({
   model: process.env.AI_MODEL,
   messages: [userMessage],
 });
